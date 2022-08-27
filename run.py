1b6f9a5bcf1741d2a767cac7b907ffa10808ff13
ocassidydev <ocassidy@tcd.ie>
2022-08-27 12:24:08 +0000

    Remove redundant colorama library and play around with curses]

--- run.py
+++ run.py
@@ -5,10 +5,12 @@ from google.oauth2.service_account import Credentials
 from urllib.request import urlopen
 import json
 #CONSOLE TOOLS
-from pyfiglet import print_figlet
-from colorama import init
-init()
-from colorama import Fore, Back, Style
+from pyfiglet import Figlet
+import curses
+from curses import wrapper
+from curses.textpad import Textbox, rectangle
+
+import time
 
 #Plugging in APIs
 #DRIVE AND SHEET
@@ -35,12 +37,114 @@ GBOOKS = "https://www.googleapis.com/books/v1/volumes?q=intitle:"
 
 #Testing console display features
 #FIGLET
-print_figlet("Hello")
+f = Figlet()
+header = f.renderText("hello")
 
-#COLORAMA
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background' + Back.RESET)
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(header)
+    stdscr.refresh()
+    stdscr.getch()
 
-print(Style.DIM + 'and in dim text')
-print(Style.BRIGHT + 'and in bright text')
-print(Style.RESET_ALL + 'back to normal now')
+    #CONFIGURE COLORS
+    # curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    # curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    # curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE)
+    # BLUE_AND_YELLOW = curses.color_pair(1)
+    # GREEN_AND_BLACK = curses.color_pair(2)
+    # RED_AND_WHITE = curses.color_pair(3)
+    
+    #ECHO USER KEYSTROKES
+    #curses.echo()
+
+    #CREATE BORDER
+    # stdscr.attron(RED_AND_WHITE)
+    # stdscr.border()
+    # stdscr.attroff(RED_AND_WHITE)
+
+    #DISPLAY RECTANGLE
+    # stdscr.attron(GREEN_AND_BLACK)
+    # rectangle(stdscr, 1, 1, 5, 20)
+    # stdscr.attroff(GREEN_AND_BLACK)
+    # stdscr.addstr(5, 30, "Hello world!")
+
+    #MOVE CURSOR
+    # stdscr.move(10, 20)
+    # stdscr.refresh()
+
+    #USE WITH ECHO TO DISPLAY USER OUTPUT
+    # while True:
+    #     key = stdscr.getkey()
+    #     if key == 'q':
+    #         break
+
+    #CREATE TEXTBOX AND GET USER INPUT
+    # win = curses.newwin(3, 18, 2, 2)
+    # box = Textbox(win)
+    # box.edit()
+    # text = box.gather().strip().replace("\n","")
+    # stdscr.addstr(10, 40, text)
+
+    #PAD
+    # pad = curses.newpad(100, 100)
+    # stdscr.refresh()
+
+    # for i in range(100):
+    #     for j in range(26):
+    #         char = chr(65 + j)
+    #         pad.addstr(char, GREEN_AND_BLACK)
+
+    #GET TERMINAL DIMENSIONS
+    #(curses.LINES -1, curses.COLS -1)
+
+    #SCROLL PADS
+    # for i in range(100):
+    #     stdscr.clear()
+    #     stdscr.refresh()
+    #     pad.refresh(i, 0, 0, 0, 20, 20)
+    #     time.sleep(0.2)
+
+    #NEW WINDOW
+    # counter_win = curses.newwin(1, 20, 10, 10)
+    # stdscr.addstr("hello world!")
+    # stdscr.refresh()
+
+    #COUNTDOWN
+    # for i in range(100):
+    #     counter_win.clear()
+    #     if i % 2 == 0:
+    #         color = GREEN_AND_BLACK
+    #     else:
+    #         color = BLUE_AND_YELLOW
+        
+    #     counter_win.addstr(f"Count: {i}", color)
+    #     counter_win.refresh()
+    #     time.sleep(0.1)
+    
+    #CURSOR
+    # x, y = 0, 0
+    # while True:
+    #     try:
+    #         key = stdscr.getkey()
+    #     except:
+    #         key = None
+        
+    #     if key == "KEY_LEFT":
+    #         if x != 0:
+    #             x -= 1
+    #     elif key == "KEY_RIGHT":
+    #         if x != curses.COLS -1:
+    #             x += 1
+    #     elif key == "KEY_UP":
+    #         if y != 0:
+    #             y -= 1
+    #     elif key == "KEY_DOWN":
+    #         if y != curses.LINES -1:
+    #             y += 1
+
+    #     stdscr.clear()
+    #     stdscr.addstr(y, x, "0")
+    #     stdscr.refresh()
+
+
+wrapper(main)
