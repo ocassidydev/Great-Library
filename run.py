3759dda97cb15e86b0796563d40d06e71347ec1a
ocassidydev <ocassidy@tcd.ie>
2022-08-31 03:40:47 +0000

    Add changed console display to white on grey to help readability on heroku

--- run.py
+++ run.py
@@ -584,7 +584,7 @@ class BrowseUI(DisplayBookMixin, ConsoleUI):
 
     def render(self):
         super().render()
-        self.scr.addstr(21, 8, "arrow right/left - navigate entries\te - edit entry\tq - quit")
+        self.scr.addstr(21, 8, "n - next\tp - prev\te - edit entry\tq - quit")
         self.scroll_books()
         return
 
@@ -867,106 +867,9 @@ def display_home(stdscr, library):
     home.render()
 
 def main(stdscr):
-    #CONFIGURE COLORS
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    GREEN_AND_BLACK = curses.color_pair(1)
-    stdscr.attron(GREEN_AND_BLACK)
-    
     name, user = display_landing(stdscr)
     library = UserLibrary(name, user)
     display_home(stdscr, library)
-
-    #ECHO USER KEYSTROKES
-    #curses.echo()
-
-    #CREATE BORDER
-    # stdscr.attron(RED_AND_WHITE)
-    # stdscr.border()
-    # stdscr.attroff(RED_AND_WHITE)
-
-    #DISPLAY RECTANGLE
-    # stdscr.attron(GREEN_AND_BLACK)
-    # rectangle(stdscr, 1, 1, 5, 20)
-    # stdscr.attroff(GREEN_AND_BLACK)
-    # stdscr.addstr(5, 30, "Hello world!")
-
-    #MOVE CURSOR
-    # stdscr.move(10, 20)
-    # stdscr.refresh()
-
-    #USE WITH ECHO TO DISPLAY USER OUTPUT
-    # while True:
-    #     key = stdscr.getkey()
-    #     if key == 'q':
-    #         break
-
-    #CREATE TEXTBOX AND GET USER INPUT
-    # win = curses.newwin(3, 18, 2, 2)
-    # box = Textbox(win)
-    # box.edit()
-    # text = box.gather().strip().replace("\n","")
-    # stdscr.addstr(10, 40, text)
-
-    #PAD
-    # pad = curses.newpad(100, 100)
-    # stdscr.refresh()
-
-    # for i in range(100):
-    #     for j in range(26):
-    #         char = chr(65 + j)
-    #         pad.addstr(char, GREEN_AND_BLACK)
-
-    #GET TERMINAL DIMENSIONS
-    #(curses.LINES -1, curses.COLS -1)
-
-    #SCROLL PADS
-    # for i in range(100):
-    #     stdscr.clear()
-    #     stdscr.refresh()
-    #     pad.refresh(i, 0, 0, 0, 20, 20)
-    #     time.sleep(0.2)
-
-    #NEW WINDOW
-    # counter_win = curses.newwin(1, 20, 10, 10)
-    # stdscr.addstr("hello world!")
-    # stdscr.refresh()
-
-    #COUNTDOWN
-    # for i in range(100):
-    #     counter_win.clear()
-    #     if i % 2 == 0:
-    #         color = GREEN_AND_BLACK
-    #     else:
-    #         color = BLUE_AND_YELLOW
-        
-    #     counter_win.addstr(f"Count: {i}", color)
-    #     counter_win.refresh()
-    #     time.sleep(0.1)
-    
-    #CURSOR
-    # x, y = 0, 0
-    # while True:
-    #     try:
-    #         key = stdscr.getkey()
-    #     except:
-    #         key = None
-        
-    #     if key == "KEY_LEFT":
-    #         if x != 0:
-    #             x -= 1
-    #     elif key == "KEY_RIGHT":
-    #         if x != curses.COLS -1:
-    #             x += 1
-    #     elif key == "KEY_UP":
-    #         if y != 0:
-    #             y -= 1
-    #     elif key == "KEY_DOWN":
-    #         if y != curses.LINES -1:
-    #             y += 1
-
-    #     stdscr.clear()
-    #     stdscr.addstr(y, x, "0")
-    #     stdscr.refresh()
 
 wrapper(main)
 
